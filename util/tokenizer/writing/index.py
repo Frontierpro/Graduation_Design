9b0196ba84d71676edd09303aab617c1a911785e
zhn <frontierpro.zhn@gmail.com>
2022-08-29 23:00:51 -0400

    get key words

---
+++ util/tokenizer/writing/index.py
@@ -0,0 +1,32 @@
+import json
+
+
+tmp_list = []
+
+for i in range(1, 70):
+    if i > 58 and i < 63:
+        continue
+    for j in range(1, 3):
+        name = 'token/' + str(i) + '-' +str(j) + '.json'
+        with open(name, 'r', encoding='utf-8') as file:
+            for each in json.load(file):
+                if j == 1:
+                    tmp_list.append([each[0], 'TPO ' + str(i) + ' Integrated'])
+                else:
+                    tmp_list.append([each[0], 'TPO ' + str(i) + ' Independent'])
+
+tmp_list = sorted(tmp_list, key=lambda item:item[0])
+
+
+index_list = []
+
+pre = ''
+for tmp in tmp_list:
+    if pre == tmp[0]:
+        index_list[len(index_list) - 1]['list'].append(tmp[1])
+    else:
+        index_list.append({'index': tmp[0], 'list': [tmp[1]]})
+        pre = tmp[0]
+
+with open('index.json', 'w', encoding='utf-8') as file:
+    file.write(json.dumps(index_list, indent=4, ensure_ascii=False))
