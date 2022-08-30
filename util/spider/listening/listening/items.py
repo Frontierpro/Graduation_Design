94f7d8cae0e0c3cd22f2165996cf44d09a43ef69
zhn <frontierpro.zhn@gmail.com>
2022-08-29 22:11:17 -0400

    listening items

---
+++ util/spider/listening/listening/items.py
@@ -0,0 +1,32 @@
+import scrapy
+
+
+class TMPItem(scrapy.Item):
+    url = scrapy.Field()
+
+
+class URLItem(scrapy.Item):
+    url = scrapy.Field()
+    num = scrapy.Field()
+    code = scrapy.Field()
+    title = scrapy.Field()
+
+
+class SETItem(scrapy.Item):
+    num = scrapy.Field()
+    code = scrapy.Field()
+    title = scrapy.Field()
+    url_list = scrapy.Field()
+    src_link = scrapy.Field()
+    text_en_us = scrapy.Field()
+    text_zh_cn = scrapy.Field()
+
+
+class SRCItem(scrapy.Item):
+    num = scrapy.Field()
+    code = scrapy.Field()
+    src_id = scrapy.Field()
+    src_link = scrapy.Field()
+    caption = scrapy.Field()
+    option = scrapy.Field()
+    answer = scrapy.Field()
