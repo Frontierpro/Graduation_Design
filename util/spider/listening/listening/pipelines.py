f3e53e327de91d345c69f9c803701c30fdebfc00
zhn <frontierpro.zhn@gmail.com>
2022-08-29 22:13:08 -0400

    listening pipelines

---
+++ util/spider/listening/listening/pipelines.py
@@ -0,0 +1,117 @@
+import json
+import copy
+from itemadapter import ItemAdapter
+
+
+class TMPPipeline:
+    def open_spider(self, spider):
+        if spider.name == 'TMP':
+            self.urls = []
+    
+    def process_item(self, item, spider):
+        if spider.name == 'TMP':
+            self.urls.append(item['url'])
+        return item
+    
+    def close_spider(self, spider):
+        if spider.name == 'TMP':
+            with open('cache/tmp/tmp.json', 'w') as file:
+                file.write(json.dumps(self.urls, indent=4))
+
+
+class URLPipeline:
+    def open_spider(self, spider):
+        if spider.name == 'URL':
+            self.items = []
+            for index in range(69):
+                self.items.append([])
+    
+    def process_item(self, item, spider):
+        if spider.name == 'URL':
+            index = int(item['num']) - 1
+            tmp = {
+                'num': int(item['num']), 'code': item['code'], 'title': item['title'], 'url': item['url']
+            }
+            self.items[index].append(tmp)
+        return item
+    
+    def close_spider(self, spider):
+        if spider.name == 'URL':
+            with open('cache/tmp/url.json', 'w') as file:
+                file.write(json.dumps(self.items, indent=4))
+
+
+class SETPipeline:
+    def open_spider(self, spider):
+        if spider.name == 'SET':
+            with open('cache/tmp/url.json', 'r') as file:
+                self.items = json.load(file)
+
+    def process_item(self, item, spider):
+        if spider.name == 'SET':
+            index = 1
+            for each in self.items[int(item['num']) - 1]:
+                if item['code'] == each['code']:
+                    break
+                index += 1
+            tmp = {
+                'title': item['title'], 'url_list': item['url_list'], 'src_link': item['src_link'],
+                'text_en_us': item['text_en_us'], 'text_zh_cn': item['text_zh_cn']
+            }
+            name = 'cache/set/' + str(int(item['num'])) + '-' + str(index) + '.json'
+            with open(name, 'w', encoding='utf-8') as file:
+                file.write(json.dumps(tmp, indent=4, ensure_ascii=False))
+        return item
+
+
+class SRCPipeline:
+    def open_spider(self, spider):
+        if spider.name == 'SRC':
+            self.tmp_list = []
+            sets = []
+            for index in range(6):
+                tmp = {'src_id': 0, 'src_link': [], 'caption': [], 'option': [], 'answer':[]}
+                sets.append(tmp)
+            section = []
+            for index in range(6):
+                section.append(copy.deepcopy(sets))
+            for index in range(69):
+                if index > 57 and index < 62:
+                    self.tmp_list.append([])
+                else:
+                    self.tmp_list.append(copy.deepcopy(section))
+
+            with open('cache/tmp/url.json', 'r') as file:
+                self.items = json.load(file)
+    
+    def process_item(self, item, spider):
+        if spider.name == 'SRC':
+            set_num = int(item['num']) - 1
+            id_num = int(item['src_id']) - 1
+            index = 1
+            for each in self.items[set_num]:
+                if item['code'] == each['code']:
+                    break
+                index += 1
+            self.tmp_list[set_num][index - 1][id_num]['src_id'] = int(item['src_id'])
+            self.tmp_list[set_num][index - 1][id_num]['src_link'] = item['src_link']
+            self.tmp_list[set_num][index - 1][id_num]['caption'] = item['caption']
+            self.tmp_list[set_num][index - 1][id_num]['option'] = item['option']
+            self.tmp_list[set_num][index - 1][id_num]['answer'] = item['answer']
+        return item
+    
+    def close_spider(self, spider):
+        if spider.name == 'SRC':
+            for section in range(69):
+                if section > 57 and section < 62:
+                    continue
+                for sets in range(6):
+                    if section > 53 and sets == 5:
+                        break
+                    name = 'cache/src/' + str(section + 1) + '-' + str(sets + 1) + '.json'
+                    with open(name, 'w', encoding='utf-8') as file:
+                        tmp = self.tmp_list[section][sets]
+                        if tmp[5]['src_id'] == 0:
+                            file.write(json.dumps(tmp[:5], indent=4, ensure_ascii=False))
+                        else:
+                            file.write(json.dumps(tmp, indent=4, ensure_ascii=False))
