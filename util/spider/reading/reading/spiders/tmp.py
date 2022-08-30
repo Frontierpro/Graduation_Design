e2e34f8a47cab84765c0ba2ebe2e6dfc1e6a4da4
zhn <frontierpro.zhn@gmail.com>
2022-08-29 22:06:22 -0400

    reading pages

---
+++ util/spider/reading/reading/spiders/tmp.py
@@ -0,0 +1,42 @@
+import scrapy
+from reading.items import TMPItem
+
+
+class TMP(scrapy.Spider):
+    name = 'TMP'
+    allowed_domains = ['liuxue.koolearn.com', 'top.zhan.com', 'toefl.kmf.com']
+    start_urls = [
+        'https://liuxue.koolearn.com/toefl/read/',
+        'https://top.zhan.com/toefl/read/alltpo.html',
+        'https://toefl.kmf.com/read/ets/new-order/0'
+    ]
+
+    def parse(self, response):
+        items = []
+        
+        xdf_filter = "//a[@class='style_search-order-btn__3uAag']/@href"
+        zhan_filter = "//ul[@class='cssTopTitleList clearfix']//a[@class='sensors_maidian']/@href"
+        kmf_filter = "//div[@class='filtrate-cont']/div[1]/div[@class='filtrate-btn']/a/@href"
+
+        xdf_urls = response.xpath(xdf_filter).extract()
+        zhan_urls = response.xpath(zhan_filter).extract()
+        kmf_urls = response.xpath(kmf_filter).extract()
+
+        for url in xdf_urls:
+            item = TMPItem()
+            item['url'] = 'https://liuxue.koolearn.com' + url
+            items.append(item)
+        items = items[3:5]
+
+        for url in zhan_urls:
+            item = TMPItem()
+            item['url'] = url
+            items.append(item)
+        items = items[:3]
+
+        for url in kmf_urls:
+            item = TMPItem()
+            item['url'] = 'https://toefl.kmf.com' + url
+            items.append(item)
+        
+        return items
