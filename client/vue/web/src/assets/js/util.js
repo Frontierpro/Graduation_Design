bb9538b1e37635087a4cef2f2215bc58a1c7ae6f
zhn <frontierpro.zhn@gmail.com>
2022-08-30 01:29:31 -0400

    utils

---
+++ client/vue/web/src/assets/js/util.js
@@ -0,0 +1,15 @@
+module.exports = {
+    encodeID: (route, title) => {
+        if (route === 'reading' || route === 'speaking')
+            return title.split(' ')[3]
+        else if (route === 'listening')
+            return title.split(' ')[2] === 'Lecture'?
+                1 + Math.floor(title.split(' ')[3] / 3) + parseInt(title.split(' ')[3]):
+                3 * title.split(' ')[3] - 2
+        else
+            return title.split(' ')[2] === 'Integrated' ? 1 : 2
+    },
+    upperHead: (tmp) => {
+        return tmp.slice(0, 1).toUpperCase() + tmp.slice(1)
+    }
+}
